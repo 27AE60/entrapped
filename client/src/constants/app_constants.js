9bd4075496244c0c0998caef1a98245d3c608d9d
ghome vic <green.rabi@hotmail.com>
2015-07-14 08:33:12 +0530

    Clean up

--- client/src/constants/app_constants.js
+++ client/src/constants/app_constants.js
@@ -1,14 +1,4 @@
 module.exports = {
-  ActionTypes : {
-    GET_MINES_LOAD: null,
-    GET_MINES_SUCCESS: null,
-    GET_MINES_FAIL: null,
-
-    LOGIN_LOAD: null,
-    LOGIN_SUCCESS: null,
-    LOGIN_FAIL: null 
-  },
-
   PayloadSources: {
     SERVER_ACTION: "SERVER_ACTION"
   }
